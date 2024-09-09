f5c34f78ded91348e8797db6f91b583ec52521b8
brahmbhatt maithili07 <8505barotmaithili@gmail.com>
2024-09-09 22:06:36 +0530

    Add files via upload

---
+++ assignment-js/assignment-1.js
@@ -0,0 +1,236 @@
+
+
+// assignment que-1
+
+//  var age=21;
+//  var name="John Doe";
+
+// // que-2
+
+// var age=21;
+// var name="john doe";
+
+// console.log(typeof age);
+// console.log(typeof name);
+
+
+// que-3
+
+// var temperature = 20;
+// console.log(typeof temperature);
+
+ // que-4
+
+// var x=5;
+// var y=3;
+
+// var add=x+y;
+// var mul=x*y;
+// var div=x/y;
+// var sub=x-y;
+
+// console.log(add,mul,div,sub);
+
+
+// que-5 (module is mostly used to check the given num is odd or even)
+
+// var  A=11;
+
+// if(A % 2 ==0){
+//     console.log("given num is even");
+
+// }
+// else{
+//     console.log("given num is odd");
+// }
+
+
+//que-6 
+
+// var a= "hello";
+// var b= "world";
+// var greeting =(a+b);
+
+// var greeting1 =(a+" "+b);
+
+// console.log(greeting);
+
+// console.log(greeting1);
+
+
+ // que-7
+
+// var balance=200;
+
+// if(balance>=1000)
+// {
+//     console.log("balance sheet is clear");
+// }
+// else 
+// {
+//      console.log("balance sheet is not clear");
+// }
+
+
+// que-8 (boolean datatype always returns two values:true and false)
+
+
+
+// var a="10";
+// var b="20";
+
+// var c=(a>b);
+// console.log(c);
+
+
+// que-9 
+
+// var x=4;
+// var y=2;
+// var z=x**y;
+
+// console.log(z);
+ 
+// var a=2;
+// var b=3;
+// var c=x**y;
+
+// console.log(z);
+ 
+// var x=10;
+// var y=0.5;
+// var z=x**y;
+
+// console.log(z);
+ 
+
+// que-10
+
+
+// var  apples=8;
+// var oranges=5;
+
+// if(apples>oranges){
+//     console.log("Apples are greater than oranges");
+// }
+
+// else{
+//     console.log("Apples are  not greater than oranges");
+// }
+
+// que-11
+
+
+// var admin="mahi";
+// var name="john";
+
+// admin=name;
+// // console.log(admin);
+
+// alert(admin);
+
+
+
+// que-12
+
+// var age=21;
+// var name="Maithili";
+// var student=true;
+
+// console.log(age,name,student);
+// console.log( typeof age, typeof name, typeof student);
+
+
+// que-13
+
+// let grade="B";
+// const name="john";
+
+// console.log(grade);
+// console.log(name);
+
+// grade="A";
+// console.log(grade);
+
+// // name="mahi";
+// // console.log(name);
+
+// que-14
+
+// let name = "Maithili";
+
+// alert(`helllo ${1}`);
+// alert(`hello ${"name"}`);
+// alert(`hello ${name}`);
+
+ // que-15
+ 
+
+// var name="Maithili";
+// console.log(name);
+
+ // que-16
+
+
+// var age=21;
+// var update=age+5;
+// console.log(age,update);
+
+
+// que-17
+
+
+// var firstname="Barot";
+// var lastname="Maithili";
+
+// var fullname=firstname+" "+lastname;
+// console.log(fullname);
+
+
+// que-18
+
+
+// var num=10;
+// var mul=num*5;
+
+// console.log(mul);
+
+
+ // que-19
+
+// var x=8;
+// var y=3;
+// var c=x+y;
+// var d=x-y;
+// var e=x*y;
+// var f=x/y;
+
+// console.log(c,d,e,f);
+
+
+// que-20
+ 
+//  var temperature=20;
+//  var temperature1="20";
+//  console.log( typeof  temperature);
+//  console.log( typeof temperature1);
+
+// que-21
+
+// var m=2;
+// var n=4;
+// var o=6;
+// var p=8;
+// var ans=((m+n)*o/(p-m));
+
+// console.log(ans);
+
+
+// que-22
+
+
+
+// var name=prompt(("Name"))
+// var age=prompt(("Age"))
+
+//  console.log("hello"+ "," +name+ " " +"your are"+" "+age+ " "+"years old!.")
